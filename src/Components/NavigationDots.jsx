0dc7777dd4737dfdff680f5b573fcf469c11ce23
nickcookies <nikos_outlook.com>
2023-09-22 21:37:54 +0300

    feat:code clean up

--- src/Components/NavigationDots.jsx
+++ src/Components/NavigationDots.jsx
@@ -3,14 +3,18 @@ import React from "react";
 const NavigationDots = ({ active }) => {
   return (
     <div className="app__navigation">
-      {["home", "about", "work", "skills", "testimonials", "contact"].map((item, index) => (
-        <a 
-          href={`#${item}`} 
-          key={item + index}
-          className="app__navigation-dot"
-          style={active === item ? {backgroundColor: '#313BAC'} : {}}
-          />
-      ))}
+      {["home", "about", "work", "skills", "testimonials", "contact"].map(
+        (item, index) => (
+          <a
+            href={`#${item}`}
+            key={item + index}
+            className="app__navigation-dot"
+            style={active === item ? { backgroundColor: "#313BAC" } : {}}
+          >
+            <div className="dots__hidden">{item}</div>
+          </a>
+        )
+      )}
     </div>
   );
 };
--- src/Constants/images.js
+++ src/Constants/images.js
@@ -1,41 +1,41 @@
-import email from '../Components/assets/email.png';
-import mobile from '../Components/assets/mobile.png';
-import api from '../Components/assets/api.png';
-import cpp from '../Components/assets/cpp.png';
-import css from '../Components/assets/css.png';
-import figma from '../Components/assets/figma.png';
-import flutter from '../Components/assets/flutter.png';
-import git from '../Components/assets/git.png';
-import graphql from '../Components/assets/graphql.png';
-import html from '../Components/assets/html.png';
-import javascript from '../Components/assets/javascript.png';
-import mu5 from '../Components/assets/mu5.png';
-import node from '../Components/assets/node.png';
-import python from '../Components/assets/python.png';
-import react from '../Components/assets/react.png';
-import redux from '../Components/assets/redux.png';
-import sass from '../Components/assets/sass.png';
-import typescript from '../Components/assets/typescript.png';
-import vue from '../Components/assets/vue.png';
+import email from "../Components/assets/email.png";
+import mobile from "../Components/assets/mobile.png";
+import api from "../Components/assets/api.png";
+import cpp from "../Components/assets/cpp.png";
+import css from "../Components/assets/css.png";
+import figma from "../Components/assets/figma.png";
+import flutter from "../Components/assets/flutter.png";
+import git from "../Components/assets/git.png";
+import graphql from "../Components/assets/graphql.png";
+import html from "../Components/assets/html.png";
+import javascript from "../Components/assets/javascript.png";
+import mu5 from "../Components/assets/mu5.png";
+import node from "../Components/assets/node.png";
+import python from "../Components/assets/python.png";
+import react from "../Components/assets/react.png";
+import redux from "../Components/assets/redux.png";
+import sass from "../Components/assets/sass.png";
+import typescript from "../Components/assets/typescript.png";
+import vue from "../Components/assets/vue.png";
 
-import about01 from '../Components/assets/about01.png';
-import about02 from '../Components/assets/about02.png';
-import about03 from '../Components/assets/about03.png';
-import about04 from '../Components/assets/about04.png';
+import about01 from "../Components/assets/about01.png";
+import about02 from "../Components/assets/about02.png";
+import about03 from "../Components/assets/about03.png";
+import about04 from "../Components/assets/about04.png";
 
-import profile from '../Components/assets/profile.png';
-import circle from '../Components/assets/circle.svg';
-import logo from '../Components/assets/logo.png';
+import profile from "../Components/assets/profile.png";
+import circle from "../Components/assets/circle.svg";
+import logo from "../Components/assets/logo.png";
 
-import adidas from '../Components/assets/adidas.png';
-import amazon from '../Components/assets/amazon.png';
-import asus from '../Components/assets/asus.png';
-import bolt from '../Components/assets/bolt.png';
-import nb from '../Components/assets/nb.png';
-import skype from '../Components/assets/skype.png';
-import spotify from '../Components/assets/spotify.png';
+import adidas from "../Components/assets/adidas.png";
+import amazon from "../Components/assets/amazon.png";
+import asus from "../Components/assets/asus.png";
+import bolt from "../Components/assets/bolt.png";
+import nb from "../Components/assets/nb.png";
+import skype from "../Components/assets/skype.png";
+import spotify from "../Components/assets/spotify.png";
 
-export default {
+const myExports = {
   email,
   mobile,
   api,
@@ -70,3 +70,5 @@ export default {
   skype,
   spotify,
 };
+
+export default myExports;
